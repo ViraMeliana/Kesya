98c12e30e42c3f826270f77cc77b8b9c0909001c
Vira Meliana <viramelianaagustin@gmail.com>
2022-06-28 21:07:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
+
 Route::redirect('/', '/login');
 
 
